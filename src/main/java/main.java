7580d9600630bbe0071afeace567402981e3da07
34674 <34674@LAPTOP-P26IH6EA>
2021-10-06 17:15:56 +0100

    Versión 2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,7 +12,28 @@ public class main {
     
     public static void main(String[] args){
         
-        int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int[] data= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        
+        Histogram histo = new Histogram(data);
+        
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        
+        for (Integer key: histogr.keySet()){
+            System.out.println(key + "==>" + histogr.get(key));
+        }
+    }
+    
+}
+
+class Histogram{
+    private final int[] data;
+    
+    public Histogram(int[] data){
+        this.data = data;
+    }
+    public int[] getData(){return this.data;}
+    
+    public Map getHistogram(){
         Map<Integer, Integer> histogram = new HashMap<>();
         
         for (int key : data) {
@@ -20,10 +41,6 @@ public class main {
             histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
             
         }
-        
-        for (Integer key: histogram.keySet()){
-            System.out.println(key + "==>" + histogram.get(key));
-        }
+        return histogram;
     }
-    
 }
