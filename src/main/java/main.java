f144406b5064c815e820b5e212d92862e77f2a19
34674 <34674@LAPTOP-P26IH6EA>
2021-10-06 17:01:45 +0100

    Versión 2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -15,13 +15,9 @@ public class main {
         int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         Map<Integer, Integer> histogram = new HashMap<>();
         
-        for (int i = 0; i < data.length; i++){
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
             
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
             
         }
         
